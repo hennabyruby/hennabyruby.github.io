18ae26d9e8fef06c8e9372f656520dfc3a5b786c
Jabran Rafique <hello@jabran.me>
2019-05-17 00:27:30 +0100

    add tags

---
+++ src/components/post/tags/index.js
@@ -0,0 +1,31 @@
+import React, { memo } from "react"
+import { Link } from "gatsby"
+
+import tagIcon from "./tag.svg"
+import tagsIcon from "./tags.svg"
+import style from "./style.module.scss"
+
+const Tags = ({ tags, isSinglePost = false }) => {
+  return (
+    <div className={isSinglePost ? style.singleView : ""}>
+      {isSinglePost && (
+        <img
+          src={tags.length === 1 ? tagIcon : tagsIcon}
+          alt=""
+          className={style.icon}
+        />
+      )}
+      {tags.map(tag => (
+        <Link
+          key={`${tag.wordpress_id}-${tag.slug}`}
+          className={style.tag}
+          to={tag.path}
+        >
+          {tag.name}
+        </Link>
+      ))}
+    </div>
+  )
+}
+
+export default memo(Tags)
